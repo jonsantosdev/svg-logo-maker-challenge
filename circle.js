2d0afdefb4028ebc5aef30f0698118df7c2c1a80
jonsantosdev <js.jsantos.jr@gmail.com>
2024-01-25 18:03:48 -0800

    cleaned up shapes and generateSVG files

--- circle.js
+++
@@ -1,9 +0,0 @@
-const Shape = require('./shape');
-
-class Circle extends Shape {
-    render() {
-      return `<circle cx="150" cy="100" r="50" ${Shape.render()} />`;
-    }
-  }
-
-module.exports = Circle;
--- generateSvg.js
+++ generateSvg.js
@@ -1,18 +1,9 @@
-// const Circle = require('./circle');
-// const Square = require('./square')
-// const Triangle = require('./triangle');
 const { Triangle, Circle, Square, Shape } = require('./lib/shapes');
 
 function generateSvg(answers) {
-// const { letters, colorText, shapes, colorShape } = answers;
+
     const shape = new Shape(answers.letters, answers.colorText, answers.colorShape);
-    
-//     const svgContent = `
-//         <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-//         <text x="50" y="50" fill="${answers.colorText}">${answers.letters}</text>
-//         <rect x="100" y="50" width="100" height="100" fill="blue" />
-//         </svg>
-//     `;
+
     if (answers.shapes === "triangle") {
         const triangle = new Triangle(answers.letters, answers.colorText, answers.colorShape)
         shape.inputShape(triangle)
